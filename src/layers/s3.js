deba3dbb364cf44fc098b7b99828ecb81be1b3e0
Eoan O Dea <odeaeoan@gmail.com>
2021-03-19 11:43:59 +0000

    S3 image path builder

--- src/layers/s3.js
+++ src/layers/s3.js
@@ -1,2 +1,2 @@
 module.exports = buildS3URL = (image) =>
-  `https://${process.env.aws_bucket_name}.s3.amazonaws.com/${image}`;
+  `https://${process.env.aws_bucket_name}.s3.amazonaws.com/upload/${image}`;
